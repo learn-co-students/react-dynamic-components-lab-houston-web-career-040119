f79ff84779b48b2f10940874d1d60861230dfa0f
PYN <yxp7531540@gmail.com>
2019-06-02 23:17:33 -0500

    done

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,12 @@
+import React, { Component } from 'react';
+
 // add Comment component here
+export default class Comment extends React.Component {
+    render() {
+        return (
+            <div className="comment">
+                {this.props.commentText}
+            </div>
+        )
+    }
+}
